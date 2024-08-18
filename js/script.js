ab73304e9b99e77bfca97b48a6d7bd97cdead1c9
Erick0105 <erickspot01@gmail.com>
2024-08-18 13:06:07 -0300

    mensagem no js concluída

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let agora = new Date();
+let hora = agora.getHours();
+let msg;
+
+if (hora > 18){
+    msg = "Boa noite";
+}else if (hora > 12){
+    msg = "Boa tarde";
+}else{
+    msg = "Bom dia";
+}
+
+document.getElementById("comprimento").textContent = msg
